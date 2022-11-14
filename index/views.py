873e238ccb707fc57cb82f3f16e264f25b0622d7
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2022-11-14 19:26:17 +0330

    Update views.py

--- index/views.py
+++ index/views.py
@@ -8,20 +8,5 @@ from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
 
-    # currency_response = requests.get("https://api.navasan.tech/latest/?api_key=freer4qaU2ifnW0lOTLdk5TQnX63X8PF")
-    # print(currency_response)
-    # currency_response_json_data = json.loads(currency_response.text)
-    # bitcoin_response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    # bitcoin_response_json_data = json.loads(bitcoin_response.text)
-    # context = {
-    #     "Dollar": currency_response_json_data["usd_buy"]["value"],
-    #     "Euro": currency_response_json_data["eur"]["value"],
-    #     "Derham": currency_response_json_data["aed"]["value"],
-    #     "Lira": currency_response_json_data["try"]["value"],
-    #     "Yuan": currency_response_json_data["cny"]["value"],
-    #     "Canadian_Dollar": currency_response_json_data["cad"]["value"],
-    #     "Pound": currency_response_json_data["gbp"]["value"],
-    #     "Bitcoin": bitcoin_response_json_data["bpi"]["USD"]["rate"],
-    # }
     context={}
     return render(request, 'index/index.html', context)
